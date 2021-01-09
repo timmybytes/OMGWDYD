260ca69c28c178e1047020a50fd5892dd9b374f7
Timothy Merritt <timmybytes@gmail.com>
2021-01-09 15:53:01 -0800

    (feat): Show repos with no commits as well as commits

--- omgwdyd.sh
+++ omgwdyd.sh
@@ -18,13 +18,17 @@ clear
 # Header
 printf -- '-%.0s' {1..65}
 echo
-printf '%s' "| LOOK AT WHAT YOU DID YESTERDAY"
-printf -- ' %.0s' {1..32}
+printf '%s' "| LOOK AT WHAT YOU DID"
+printf -- ' %.0s' {1..42}
 printf '%s\n' "|"
 
+# Change this variable to the parent folder of your repos
+repos="$HOME/Projects/\#Repos/*"
+
+shame=()
+
 # For each directory in #Repos
-# ---> Change this line to the parent folder of your repos <---
-for dir in ~/Projects/\#Repos/*; do
+for dir in ${repos}; do
   # If dir is a directory, cd into it and silence errors (for non-directories)
   cd "$dir" 2>/dev/null || exit
   # If directory is git repo, continue; else, next loop iteration
@@ -41,6 +45,7 @@ for dir in ~/Projects/\#Repos/*; do
       # Print git log since yesterday, show only commits
       git --no-pager log --since="yesterday" --pretty=tformat:"%x20*%x20%x20%s"
     else
+      shame+=(${PWD##*/})
       continue
     fi
   else
@@ -50,3 +55,23 @@ for dir in ~/Projects/\#Repos/*; do
   fi
   cd ..
 done
+
+# Show repos with NO commits in past 24 hours
+printf -- '-%.0s' {1..65}
+echo
+printf '%s' "| LOOK AT WHAT YOU DIDN'T DO"
+printf -- ' %.0s' {1..36}
+printf '%s\n' "|"
+printf -- '-%.0s' {1..65}
+echo
+printf '%s\n' "// Unloved repositories: "
+for id in ${shame[@]}; do
+  # printf -- '-%.0s' {1..65}
+  # Print name of current directory removed from full filepath
+  printf -- ' %.0s' {1..5}
+  printf '%s\n' "${id}"
+  # Print git log since yesterday, show only commits
+  # printf -- '-%.0s' {1..65}
+done
+printf -- '-%.0s' {1..65}
+echo
